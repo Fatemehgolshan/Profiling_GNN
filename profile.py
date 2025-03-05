34effc57bb9f2a4f82cdc8dc4ef94e80024dec67
Fatemehgolshan <fag28@pitt.edu>
2025-03-04 23:45:28 -0500

    Generalized profile for long-term use

--- profile.py
+++ profile.py
@@ -1,12 +1,16 @@
 import geni.portal as portal
 import geni.rspec.pg as rspec
 
+# Define parameters
+portal.context.defineParameter("node_type", "Select Node Type", portal.ParameterType.STRING, "d7525")
+
+# Create Request
 request = portal.Context().makeRequestRSpec()
 
-# Create a single node in Wisconsin cluster
+# Create a node
 node = request.RawPC("node1")
-node.hardware_type = "d7525"
+node.hardware_type = portal.context.bindParameter("node_type")
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops//UBUNTU22-64-STD"
 
-# Output the RSpec
+# Output RSpec
 portal.context.printRequestRSpec()
